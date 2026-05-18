49a5f7f04f1d53ae9b51e872798cb4b4606e5d31
MeezanOne <meezan@elred.io>
2026-05-18 11:04:07 +0530

    a

--- src/store/useDatepicker.js
+++ src/store/useDatepicker.js
@@ -1,34 +1,25 @@
 import { create } from "zustand";
 
 export const useDatepicker = create((set) => ({
-  isOpen: false,
+  isOpen: false,
+  selectedDate: new Date(),
+  hoveredDate: null,
+  onConfirm: null,
 
-  viewDate: new Date(),
-  selectedDate: null,
+  openDatepicker: ({ date = new Date(), onConfirm }) =>
+    set({
+      isOpen: true,
+      selectedDate: date,
+      onConfirm,
+    }),
 
-  hoveredDate: null,
+  closeDatepicker: () =>
+    set({
+      isOpen: false,
+      hoveredDate: null,
+      onConfirm: null,
+    }),
 
-  openDatepicker(date = new Date()) {
-    set({
-      isOpen: true,
-      viewDate: date,
-      selectedDate: date,
-    });
-  },
-
-  closeDatepicker() {
-    set({ isOpen: false, hoveredDate: null });
-  },
-
-  setViewDate(date) {
-    set({ viewDate: date });
-  },
-
-  setSelectedDate(date) {
-    set({ selectedDate: date });
-  },
-
-  setHoveredDate(day) {
-    set({ hoveredDate: day });
-  },
+  setDate: (date) => set({ selectedDate: date }),
+  setHoveredDate: (date) => set({ hoveredDate: date }),
 }));
