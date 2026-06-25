5c9ebaf8ca1b759832a2830e0bb32507903ab25f
TashafM <thecomputerpitch@gmail.com>
2026-06-25 17:29:05 +0530

    new

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,7 @@ import { Suspense } from "react";
 import Login from "./Login";
 
 export default function Page() {
+  console.log('dev env')
   return (
     <Suspense fallback={null}>
       <Login />
