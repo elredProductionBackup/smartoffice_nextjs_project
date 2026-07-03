8362c90c484f34240f51ac202fb07e7556444ff4
SRayyan555 <rayyan@elred.io>
2026-07-03 11:27:53 +0530

    portfolio officer page

---
+++ src/app/dashboard/portfolio-officer/page.jsx
@@ -0,0 +1,11 @@
+'use client';
+
+const PortfolioOfficerPage = () => {
+  return (
+    <div className="p-6">
+      <h1 className="text-[28px] font-bold text-[#333333]">Portfolio Officer</h1>
+    </div>
+  );
+};
+
+export default PortfolioOfficerPage;
